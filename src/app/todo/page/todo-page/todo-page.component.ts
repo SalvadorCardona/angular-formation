45dbc6ed082ef5318aad2d283ea26297d3592277
Salva <hebustha@gmail.com>
2022-04-06 15:55:13 +0200

    Modèle est utilisé

--- src/app/todo/page/todo-page/todo-page.component.ts
+++ src/app/todo/page/todo-page/todo-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../model/todo';
 
 @Component({
   selector: 'app-todo-page',
   templateUrl: './todo-page.component.html',
-  styleUrls: ['./todo-page.component.scss']
+  styleUrls: ['./todo-page.component.scss'],
 })
 export class TodoPageComponent implements OnInit {
+  todos: Todo[] = [
+    { content: 'Tache 1' },
+    { content: 'Tache 2' },
+    { content: 'Tache 3' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
