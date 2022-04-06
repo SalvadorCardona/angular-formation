64fc204e954cf046746d68189c481031914fba7a
Salva <hebustha@gmail.com>
2022-04-06 15:25:06 +0200

    Conteneur est créé

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoPageComponent } from './page/todo-page/todo-page.component';
+
+@NgModule({
+  declarations: [TodoPageComponent],
+  imports: [CommonModule],
+})
+export class TodoModule {}
