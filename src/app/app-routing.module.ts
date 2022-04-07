a46966e04167d1f47f7478bdc0ad963a23f59878
Salva <hebustha@gmail.com>
2022-04-07 17:42:29 +0200

    Finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Nous importons notre page composant dans le routeur
 import { TodoPageComponent } from './todo/page/todo-page/todo-page.component';
 
-const routes: Routes = [{ path: 'todo', component: TodoPageComponent }];
+const routes: Routes = [
+  // Affiche le composant TodoPageComponent quand l'url est todo
+  { path: 'todo', component: TodoPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
