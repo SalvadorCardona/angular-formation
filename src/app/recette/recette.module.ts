adcbe9cd31da33fcba90020b0f29d23540d4e286
Salva <hebustha@gmail.com>
2022-04-09 13:51:09 +0200

    Updates

--- src/app/recette/recette.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RecetteComponent } from './page/recette/recette.component';
-
-@NgModule({
-  declarations: [RecetteComponent],
-  imports: [CommonModule],
-})
-export class RecetteModule {}
--- src/app/todo/model/todo.ts
+++
@@ -1,4 +0,0 @@
-export interface Todo {
-  // Notre todo va contenir du text dans la variable content
-  content: string;
-}
