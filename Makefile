a165903f0f99b3eaedad3caa0e6a99ee25162795
Salvador <hebustha@gmail.com>
2022-04-12 18:10:46 +0200

    Finish
    Add Action
    
    CC
    
    CC
    
    CC
    
    CC
    
    CC
    
    CC
    
    cc
    
    cc
    
    cc
    
    DD
    
    
    dd
    
    
    dd
    
    DD
    
    Fix Mots
    
    Fix Mots
    
    Fix CI
    
    Fix CI
    
    Fix CI
    
    dd

---
+++ Makefile
@@ -0,0 +1,5 @@
+server-dev:
+	ng serve
+
+deploy:
+	npm run deploy
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecetteComponent } from './recette/page/recette/recette.component';
 // Nous importons notre page composant dans le routeur
 import { TodoPageComponent } from './todo/page/todo-page/todo-page.component';
 
 const routes: Routes = [
   // Affiche le composant TodoPageComponent quand l'url est todo
   { path: 'todo', component: TodoPageComponent },
+  { path: 'recette', component: RecetteComponent },
 ];
 
 @NgModule({
