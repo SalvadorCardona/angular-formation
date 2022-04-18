6c3dba95ca1c0340f3e7a9fb4d82cae093c6772d
Salva <hebustha@gmail.com>
2022-04-18 14:09:39 +0200

    Ajouter le compteur module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompteurComponent } from './compteur/page/compteur/compteur.component';
 import { RecetteComponent } from './recette/page/recette/recette.component';
 // Nous importons notre page composant dans le routeur
 import { TodoPageComponent } from './todo/page/todo-page/todo-page.component';
@@ -7,7 +8,10 @@ import { TodoPageComponent } from './todo/page/todo-page/todo-page.component';
 const routes: Routes = [
   // Affiche le composant TodoPageComponent quand l'url est todo
   { path: 'todo', component: TodoPageComponent },
+  // Affiche le composant RecetteComponent quand l'url est recette
   { path: 'recette', component: RecetteComponent },
+  // Affiche le composant CompteurComponent quand l'url est compteur
+  { path: 'compteur', component: CompteurComponent },
 ];
 
 @NgModule({
